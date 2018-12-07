9fc366c92b15f3b08bd0e9b74a822ccbf82a110b
Chris J. Karr <chris@audacious-software.com>
2018-12-07 11:20:55 -0600

    Initial setup for Travis CI

---
+++ QuestionKit/QuestionKit.h
@@ -0,0 +1,19 @@
+//
+//  QuestionKit.h
+//  QuestionKit
+//
+//  Created by Chris Karr on 12/7/18.
+//  Copyright © 2018 Audacious Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QuestionKit.
+FOUNDATION_EXPORT double QuestionKitVersionNumber;
+
+//! Project version string for QuestionKit.
+FOUNDATION_EXPORT const unsigned char QuestionKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuestionKit/PublicHeader.h>
+
+
