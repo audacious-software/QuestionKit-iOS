c4e0266313bc686701e7044043fa978995cdd25a
Chris J. Karr <chris@audacious-software.com>
2019-09-09 13:17:46 -0500

    Implemented year selection card

---
+++ QuestionKit/Source/Views/SelectYearCardView.h
@@ -0,0 +1,19 @@
+//
+//  SelectYearCardView.h
+//  QuestionKit
+//
+//  Created by Chris Karr on 12/20/18.
+//  Copyright © 2018 Audacious Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SingleLineTextCardView.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SelectYearCardView : SingleLineTextCardView
+
+@end
+
+NS_ASSUME_NONNULL_END
